8b9201538cd4dc853b1d44c17adceb0587c6efbb
raoof hojat <ra0o0f@yahoo.com>
2016-09-03 14:50:54 +0430

    added simple query examples

--- src/ArangoDB.Client.Examples/SimpleQueries/SimpleQueryCommand.cs
+++ src/ArangoDB.Client.Examples/SimpleQueries/SimpleQueryCommand.cs
@@ -12,18 +12,14 @@ namespace ArangoDB.Client.Examples.SimpleQueries
     {
         List<Person> InsertSomePerson()
         {
-            // first remove all persons
-            db.Query<Person>().Remove().Execute();
-
             List<Person> persons = new List<Person>()
             {
                 new Person { Name = "raoof",Age = 28 },
                 new Person { Name = "Dr. frank celler",Age=30 },
-                new Person {Name = "hojat",Age=27 }
+                new Person { Name = "hojat",Age=27 }
             };
 
-            foreach (var p in persons)
-                db.Insert<Person>(p);
+            db.InsertMultiple<Person>(persons);
 
             return persons;
         }
@@ -31,19 +27,48 @@ namespace ArangoDB.Client.Examples.SimpleQueries
         [Fact]
         public void All()
         {
+            ClearDatabase();
+
             var persons = InsertSomePerson();
 
-            var loadedPersons = db.All<Person>().ToList();
+            var cursor = db.All<Person>();
+
+            var loadedPersons = cursor.ToList();
+
+            Assert.Equal(cursor.Statistics.RequestCount, 1);
 
             Assert.Equal(loadedPersons.Count, 3);
 
-            Assert.Empty(loadedPersons.Select(p => p.Key)
-                .Except(persons.Select(p=>p.Key)));
+            Assert.Empty(loadedPersons
+                .Select(p => p.Key)
+                .Except(persons.Select(p => p.Key)));
+        }
+
+        [Fact]
+        public void AllWithBatch()
+        {
+            ClearDatabase();
+
+            var persons = InsertSomePerson();
+
+            var cursor = db.All<Person>(batchSize: 1);
+
+            var loadedPersons = cursor.ToList();
+
+            Assert.Equal(cursor.Statistics.RequestCount, 3);
+
+            Assert.Equal(loadedPersons.Count, 3);
+
+            Assert.Empty(loadedPersons
+                .Select(p => p.Key)
+                .Except(persons.Select(p => p.Key)));
         }
 
         [Fact]
         public void Any()
         {
+            ClearDatabase();
+
             var persons = InsertSomePerson();
 
             var person = db.Any<Person>();
@@ -54,12 +79,39 @@ namespace ArangoDB.Client.Examples.SimpleQueries
         [Fact]
         public void ByExample()
         {
+            ClearDatabase();
+
             var persons = InsertSomePerson();
 
             var loadedPersons = db.ByExample<Person>(new { Name = "Dr. frank celler" }).ToList();
 
             Assert.Equal(loadedPersons.Count, 1);
             Assert.Equal(loadedPersons[0].Key, persons.First(p => p.Name == "Dr. frank celler").Key);
+        }
+
+        [Fact]
+        public void FirstExample()
+        {
+            ClearDatabase();
+
+            var persons = InsertSomePerson();
+
+            var loadedPerson = db.FirstExample<Person>(new { Name = "Dr. frank celler" });
+
+            Assert.NotNull(loadedPerson);
+            Assert.Equal(loadedPerson.Key, persons.First(p => p.Name == "Dr. frank celler").Key);
+        }
+
+        [Fact]
+        public void Range()
+        {
+            ClearDatabase();
+
+            var persons = InsertSomePerson();
+
+            var loadedPerson = db.Range<Person>(x => x.Age, 25, 28, closed: true).ToList();
+            
+            Assert.Equal(loadedPerson.Count, 2);
         }
     }
 }
