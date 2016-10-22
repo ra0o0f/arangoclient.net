a625bb866afea93cfa4683a0cf4177ff7ba394c3
raoof hojat <ra0o0f@yahoo.com>
2016-10-22 11:39:01 +0330

    v0.7.60

--- src/ArangoDB.Client/Properties/AssemblyInfo.cs
+++ src/ArangoDB.Client/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.50")]
-[assembly: AssemblyFileVersion("0.7.50")]
+[assembly: AssemblyVersion("0.7.60")]
+[assembly: AssemblyFileVersion("0.7.60")]
 [assembly: CLSCompliant(true)]
