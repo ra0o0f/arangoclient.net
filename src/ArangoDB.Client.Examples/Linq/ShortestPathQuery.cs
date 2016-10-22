19ed9b7903e9e9914534ae68bef08d25254d5191
raoof hojat <ra0o0f@yahoo.com>
2016-10-22 10:48:41 +0330

    added shortestpath query examples & tests

---
+++ src/ArangoDB.Client.Examples/Linq/ShortestPathQuery.cs
@@ -0,0 +1,198 @@
+﻿using ArangoDB.Client.Data;
+using ArangoDB.Client.Examples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArangoDB.Client.Examples.Linq
+{
+    public class ShortestPathQuery : TestDatabaseSetup
+    {
+        Person alice = new Person
+        {
+            Name = "Alice",
+            Age = 21
+        };
+
+        Person bob = new Person
+        {
+            Name = "Bob",
+            Age = 22
+        };
+
+        Person charlie = new Person
+        {
+            Name = "Charlie",
+            Age = 23
+        };
+
+        Person dave = new Person
+        {
+            Name = "Dave",
+            Age = 24
+        };
+
+        Person eve = new Person
+        {
+            Name = "Eve",
+            Age = 24
+        };
+
+        Follow aliceBob;
+        Follow bobCharlie;
+        Follow bobDave;
+        Follow charlieEve;
+
+        string graphName = "SocialGraph";
+
+        private void InitiateGraph()
+        {
+            db.InsertMultiple<Person>(new Person[]
+            {
+                alice, bob, charlie, dave, eve
+            });
+
+            aliceBob = new Follow
+            {
+                Follower = alice.Id,
+                Followee = bob.Id,
+                Label = nameof(aliceBob)
+            };
+            bobCharlie = new Follow
+            {
+                Follower = bob.Id,
+                Followee = charlie.Id,
+                Label = nameof(bobCharlie)
+            };
+            bobDave = new Follow
+            {
+                Follower = bob.Id,
+                Followee = dave.Id,
+                Label = nameof(bobDave)
+            };
+            charlieEve = new Follow
+            {
+                Follower = charlie.Id,
+                Followee = eve.Id,
+                Label = nameof(charlieEve)
+            };
+
+            db.InsertMultiple<Follow>(new Follow[]
+            {
+                aliceBob, bobCharlie, bobDave, charlieEve
+            });
+
+            db.Graph(graphName).Create(new EdgeDefinitionTypedData[]
+            {
+                new EdgeDefinitionTypedData
+                {
+                    Collection = typeof(Follow),
+                    From = new Type[] { typeof(Person) },
+                    To = new Type[] { typeof(Person) }
+                }
+            });
+        }
+
+        [Fact]
+        public void ShortestPath()
+        {
+            InitiateGraph();
+
+            var result = db.Query()
+                .ShortestPath<Person, Follow>(alice.Id, eve.Id)
+                .OutBound()
+                .Graph(graphName)
+                .Select(g => g)
+                .ToList();
+
+            Assert.Equal(result.Count, 4);
+            Assert.Equal(result[0].Vertex.Key, alice.Key);
+            Assert.Equal(result[1].Vertex.Key, bob.Key);
+            Assert.Equal(result[2].Vertex.Key, charlie.Key);
+            Assert.Equal(result[3].Vertex.Key, eve.Key);
+
+            Assert.Null(result[0].Edge);
+            Assert.Equal(result[1].Edge.Key, aliceBob.Key);
+            Assert.Equal(result[2].Edge.Key, bobCharlie.Key);
+            Assert.Equal(result[3].Edge.Key, charlieEve.Key);
+        }
+
+        [Fact]
+        public void ShortestPathInbound()
+        {
+            InitiateGraph();
+
+            var result = db.Query()
+                .ShortestPath<Person, Follow>(eve.Id, alice.Id)
+                .InBound()
+                .Graph(graphName)
+                .Select(g => g)
+                .ToList();
+
+            Assert.Equal(result.Count, 4);
+        }
+
+        [Fact]
+        public void ShortestPathInvalidGraphName()
+        {
+            InitiateGraph();
+
+            var query = db.Query()
+                .ShortestPath<Person, Follow>(eve.Id, alice.Id)
+                .InBound()
+                .Graph("InvalidGraphName")
+                .Select(g => g);
+
+            Assert.Throws<ArangoServerException>(() => query.ToList());
+        }
+
+        [Fact]
+        public void ShortestPathSelectVertices()
+        {
+            InitiateGraph();
+
+            var result = db.Query()
+                .ShortestPath<Person, Follow>(eve.Id, alice.Id)
+                .Graph(graphName)
+                .Select(g => g.Vertex.Key)
+                .ToList();
+
+            Assert.Equal(result.Count, 4);
+            Assert.True(result.All(x => string.IsNullOrEmpty(x) == false));
+        }
+
+        [Fact]
+        public void ShortestPathInEdges()
+        {
+            InitiateGraph();
+
+            var result = db.Query()
+                .ShortestPath<Person, Follow>(eve.Id, alice.Id)
+                .InBound()
+                .Edge(db.NameOf<Follow>())
+                .Select(g => g)
+                .ToList();
+
+            Assert.Equal(result.Count, 4);
+        }
+
+
+        [Fact]
+        public void ShortestPathInEdgesInBound()
+        {
+            InitiateGraph();
+
+            var result = db.Query()
+                .ShortestPath<Person, Follow>(eve.Id, alice.Id)
+                .OutBound()
+                .Edge(db.NameOf<Follow>(), EdgeDirection.Inbound)
+                .Select(g => g)
+                .ToList();
+
+            Assert.Equal(result.Count, 4);
+        }
+    }
+}
---
+++ src/ArangoDB.Client.Test/Linq/ShortestPathQuery.cs
@@ -0,0 +1,202 @@
+﻿using ArangoDB.Client.Test.Database;
+using ArangoDB.Client.Test.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ArangoDB.Client.Test.Utility;
+
+namespace ArangoDB.Client.Test.Linq
+{
+    public class ShortestPathQuery
+    {
+        [Fact]
+        public void ShortestPath()
+        {
+            var db = DatabaseGenerator.Get();
+
+            var query = db.Query()
+                .ShortestPath<Person, Friend>("Person/1234", "Person/4321")
+                .OutBound()
+                .Graph("SocialGraph")
+                .Select(g => g)
+                .GetQueryData();
+
+            Assert.Equal(query.Query.RemoveSpaces(), @"
+for `graph_0_Vertex`, `graph_0_Edge` 
+in outbound shortest_path @P1 to @P2 graph ""SocialGraph""
+return { `vertex` : `graph_0_Vertex`, `edge` : `graph_0_Edge` }".RemoveSpaces());
+
+            Assert.Equal(query.BindVars[0].Value, "Person/1234");
+            Assert.Equal(query.BindVars[1].Value, "Person/4321");
+        }
+
+        [Fact]
+        public void ShortestPathOutBoundDirection()
+        {
+            var db = DatabaseGenerator.Get();
+
+            var query = db.Query()
+                .ShortestPath<Person, Friend>("Person/1234", "Person/4321")
+                .OutBound()
+                .Graph("SocialGraph")
+                .Select(g => g)
+                .GetQueryData();
+
+            Assert.Equal(query.Query.RemoveSpaces(), @"
+for `graph_0_Vertex`, `graph_0_Edge`
+in outbound shortest_path @P1 to @P2 graph ""SocialGraph""
+return { `vertex` : `graph_0_Vertex`, `edge` : `graph_0_Edge` }".RemoveSpaces());
+        }
+
+        [Fact]
+        public void ShortestPathInBoundDirection()
+        {
+            var db = DatabaseGenerator.Get();
+
+            var query = db.Query()
+                .ShortestPath<Person, Friend>("Person/1234", "Person/4321")
+                .InBound()
+                .Graph("SocialGraph")
+                .Select(g => g)
+                .GetQueryData();
+
+            Assert.Equal(query.Query.RemoveSpaces(), @"
+for `graph_0_Vertex`, `graph_0_Edge`
+in inbound shortest_path @P1 to @P2 graph ""SocialGraph""
+return { `vertex` : `graph_0_Vertex`, `edge` : `graph_0_Edge` }".RemoveSpaces());
+        }
+
+        [Fact]
+        public void ShortestPathAnyDirection()
+        {
+            var db = DatabaseGenerator.Get();
+
+            var query = db.Query()
+                .ShortestPath<Person, Friend>("Person/1234", "Person/4321")
+                .AnyDirection()
+                .Graph("SocialGraph")
+                .Select(g => g)
+                .GetQueryData();
+
+            Assert.Equal(query.Query.RemoveSpaces(), @"
+for `graph_0_Vertex`, `graph_0_Edge` 
+in any shortest_path @P1 to @P2 graph ""SocialGraph""
+return { `vertex` : `graph_0_Vertex`, `edge` : `graph_0_Edge` }".RemoveSpaces());
+        }
+
+        [Fact]
+        public void ShortestPathWithoutDirection()
+        {
+            var db = DatabaseGenerator.Get();
+
+            var query = db.Query()
+                .ShortestPath<Person, Friend>("Person/1234", "Person/4321")
+                .Graph("SocialGraph")
+                .Select(g => g)
+                .GetQueryData();
+
+            Assert.Equal(query.Query.RemoveSpaces(), @"
+for `graph_0_Vertex`, `graph_0_Edge` 
+in any shortest_path @P1 to @P2 graph ""SocialGraph""
+return { `vertex` : `graph_0_Vertex`, `edge` : `graph_0_Edge` }".RemoveSpaces());
+        }
+
+        [Fact]
+        public void ShortestPathSelectVertexMember()
+        {
+            var db = DatabaseGenerator.Get();
+
+            var query = db.Query()
+                .ShortestPath<Person, Friend>("Person/1234", "Person/4321")
+                .Graph("SocialGraph")
+                .Select(g => g.Vertex.Age)
+                .GetQueryData();
+
+            Assert.Equal(query.Query.RemoveSpaces(), @"
+for `graph_0_Vertex`, `graph_0_Edge`
+in any shortest_path @P1 to @P2 graph ""SocialGraph""
+return `graph_0_Vertex`.`Age`".RemoveSpaces());
+        }
+
+        [Fact]
+        public void ShortestPathSelectEdgeMember()
+        {
+            var db = DatabaseGenerator.Get();
+
+            var query = db.Query()
+                .ShortestPath<Person, Friend>("Person/1234", "Person/4321")
+                .Graph("SocialGraph")
+                .Select(g => g.Edge.Key)
+                .GetQueryData();
+
+            Assert.Equal(query.Query.RemoveSpaces(), @"
+for `graph_0_Vertex`, `graph_0_Edge` 
+in any shortest_path @P1 to @P2 graph ""SocialGraph""
+return `graph_0_Edge`.`_key`".RemoveSpaces());
+        }
+        
+        [Fact]
+        public void ShortestPathOptions()
+        {
+            var db = DatabaseGenerator.Get();
+
+            var query = db.Query()
+                .ShortestPath<Person, Friend>("Person/1234", "Person/4321")
+                .OutBound()
+                .Graph("SocialGraph")
+                .Options(new { bfs = true })
+                .Select(g => g)
+                .GetQueryData();
+
+            Assert.Equal(query.Query.RemoveSpaces(), @"
+for `graph_0_Vertex`, `graph_0_Edge`
+in outbound shortest_path @P1 to @P2 graph ""SocialGraph""  options {""bfs"":true}
+return { `vertex` : `graph_0_Vertex`, `edge` : `graph_0_Edge` }".RemoveSpaces());
+        }
+
+        [Fact]
+        public void ShortestPathInEdges()
+        {
+            var db = DatabaseGenerator.Get();
+
+            var query = db.Query()
+                .ShortestPath<Person, Friend>("Person/1234", "Person/4321")
+                .OutBound()
+                .Edge(db.NameOf<Friend>())
+                .Edge(db.NameOf<Flight>())
+                .Select(g => g)
+                .GetQueryData();
+
+            Assert.Equal(query.Query.RemoveSpaces(), @"
+for `graph_0_Vertex`, `graph_0_Edge` 
+in outbound shortest_path @P1 to @P2 `Friend`, `Flight`
+return { `vertex` : `graph_0_Vertex`, `edge` : `graph_0_Edge` }".RemoveSpaces());
+
+            Assert.Equal(query.BindVars[0].Value, "Person/1234");
+        }
+
+        [Fact]
+        public void ShortestPathInEdgesWithDirection()
+        {
+            var db = DatabaseGenerator.Get();
+
+            var query = db.Query()
+                .ShortestPath<Person, Friend>("Person/1234", "Person/4321")
+                .OutBound()
+                .Edge(db.NameOf<Friend>(), EdgeDirection.Inbound)
+                .Edge(db.NameOf<Flight>())
+                .Select(g => g)
+                .GetQueryData();
+
+            Assert.Equal(query.Query.RemoveSpaces(), @"
+for `graph_0_Vertex`, `graph_0_Edge`
+in outbound shortest_path @P1 to @P2 inbound `Friend`, `Flight`
+return { `vertex` : `graph_0_Vertex`, `edge` : `graph_0_Edge` }".RemoveSpaces());
+
+            Assert.Equal(query.BindVars[0].Value, "Person/1234");
+        }
+    }
+}
