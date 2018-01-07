4a88d229af647585f246e878e615ff4e9600529e
Raoof Hojat <ra0o0f@yahoo.com>
2018-01-07 11:57:36 +0330

    database option init

---
+++ src/ArangoDB.Client/Option/DatabaseOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArangoDB.Client.Option
+{
+    public class DatabaseOption
+    {
+        public bool WaitForSync { get; set; }
+
+        public bool ThrowForServerErrors { get; set; }
+
+        public bool DisableChangeTracking { get; set; }
+    }
+}
---
+++ src/ArangoDB.Client/Option/DatabaseOptionBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArangoDB.Client.Option
+{
+    public class DatabaseOptionBuilder
+    {
+        DatabaseOption _option;
+
+        public DatabaseOptionBuilder(DatabaseOption option)
+        {
+            _option = option;
+        }
+
+        public DatabaseOption Option => _option;
+
+        public DatabaseOptionBuilder WaitForSync(bool value = true)
+        {
+            _option.WaitForSync = value;
+            return this;
+        }
+
+        public DatabaseOptionBuilder ThrowForServerErrors(bool value = true)
+        {
+            _option.ThrowForServerErrors = true;
+            return this;
+        }
+
+        public DatabaseOptionBuilder DisableChangeTracking(bool value = true)
+        {
+            _option.DisableChangeTracking = true;
+            return this;
+        }
+    }
+}
